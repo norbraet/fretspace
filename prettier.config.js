1e5e44f29504defcc8b50b67ffedc7af999215c5
Norbert Balaz <n.balaz@outlook.com>
2026-02-04 22:47:06 +0100

    setup: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,27 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: false,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 100,
+  endOfLine: "lf",
+  vueIndentScriptAndStyle: true,
+  overrides: [
+    {
+      files: "*.json",
+      options: {
+        printWidth: 120,
+      },
+    },
+    {
+      files: "*.md",
+      options: {
+        printWidth: 80,
+        proseWrap: "preserve",
+      },
+    },
+  ],
+};
